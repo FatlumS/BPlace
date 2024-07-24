a1faf30e64fae9cf513958e87134b91405f8b4c7
FatlumS <98318204+FatlumS@users.noreply.github.com>
2024-07-24 15:22:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Scroll Behavior
+let lastScrollTop = 0; // Keeps track of the last scroll position
+const header = document.querySelector('header'); // Select the header element
+
+window.addEventListener('scroll', function() {
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+        // Scrolling down
+        header.classList.add('hide-header');
+    } else {
+        // Scrolling up
+        header.classList.remove('hide-header');
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+});
+
+// Login and Registration Popup
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -6,17 +25,16 @@ const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
-  });
+});
 
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
+
 loginPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-
-
-  iconClose.addEventListener('click', () => {
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
-  });
+});
